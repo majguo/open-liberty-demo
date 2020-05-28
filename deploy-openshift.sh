8ed579b3295d8cec72ba27066c15c55eed012566
majguo <jiangma@microsoft.com>
2020-05-28 18:26:29 +0800

    enable TLS for demo app using OpenLibertyApplication so it can be deployed to openshift

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -3,8 +3,8 @@
 export APPLICATION_IMAGE=${1}
 export ELASTIC_CLOUD_ID=${2}
 export ELASTIC_CLOUD_AUTH=${3}:${4}
-export KEYSTORE_NAME=${5}
-export KEYSTORE_PASSWORD=${6}
+CA_CRT_NAME=${5}
+CA_KEY_NAME=${6}
 export POSTGRESQL_SERVER_NAME=${7}
 export POSTGRESQL_USER=${8}
 export POSTGRESQL_PASSWORD=${9}
@@ -18,10 +18,19 @@ oc create serviceaccount -n ${NAMESPACE} privileged-account
 oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z privileged-account
 export SERVICE_ACCOUNT_NAME=privileged-account
 
+# create secret including tls.crt & tls.key
+# note:
+# - namespace "cert-manager" must be created before when installing cert-manager operator
+# - "CA_CRT_NAME" for CA certificate & "CA_KEY_NAME" for CA private key must be generated ahead of time,
+#   e.g., "openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout ca.key -out ca.crt"
+oc create secret generic ca-secret --from-file=tls.crt=./deploy/${CA_CRT_NAME} --from-file=tls.key=./deploy/${CA_KEY_NAME} -n cert-manager
+export CA_SECRET_NAME=ca-secret
+export CLUSTER_ISSUER_NAME=openliberty-demo-ca
+
+envsubst < deploy/ca-clusterissuer.yaml | oc apply -n ${NAMESPACE} -f -
 envsubst < deploy/elastic-cloud-secret.yaml | oc apply -n ${NAMESPACE} -f -
 oc apply -n ${NAMESPACE} -f deploy/filebeat-elastic-hosted.yaml
-envsubst < deploy/keystore.yaml | oc apply -n ${NAMESPACE} -f -
 envsubst < deploy/postgresql-secret.yaml | oc apply -n ${NAMESPACE} -f -
 envsubst < deploy/aad-oidc-secret.yaml | oc apply -n ${NAMESPACE} -f -
-envsubst < deploy/openlibertyapplication.yaml | oc apply -n ${NAMESPACE} -f -
+envsubst < deploy/openlibertyapplication-openshift.yaml | oc apply -n ${NAMESPACE} -f -
 echo "The application is succesfully deployed to project ${NAMESPACE}!"
