064efe628fd6dd287dd93045f1258992aad66e1f
Jianguo Ma <jiangma@microsoft.com>
2020-08-03 13:49:38 +0800

    deploy sample to aks cluster using OpenLibertyApplication CR

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -13,6 +13,7 @@ export DB_USER=${9}
 export DB_PASSWORD=${10}
 export ELASTIC_CLOUD_ID=${11}
 export ELASTIC_CLOUD_AUTH=${12}
+useOperator=${13}
 
 # Create Namespace "open-liberty-demo"
 NAMESPACE=open-liberty-demo
@@ -30,7 +31,25 @@ kubectl create -f deploy/filebeat-config.yaml
 # Create Secret "elastic-cloud-secret"
 envsubst < deploy/elastic-cloud-secret.yaml | kubectl create -f -
 
-# Create Deployment & Service instances which connects to hosted elasticsearch service 
-envsubst < deploy/k8s-hosted-elasticsearch.yaml | kubectl create -f -
+# Create Deployment & Service instances which connects to hosted elasticsearch service
+if [ "$useOperator" != false ]; then
+    # Create TLS private key and certificate, which is also used as CA certificate for testing purpose
+    openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt
+
+    # Create environment variables which will be passed to secret "tls-crt-secret"
+    export CA_CRT=$(cat tls.crt | base64 -w 0)
+    export DEST_CA_CRT=$(cat tls.crt | base64 -w 0)
+    export TLS_CRT=$(cat tls.crt | base64 -w 0)
+    export TLS_KEY=$(cat tls.key | base64 -w 0)
+
+    # Create secret "tls-crt-secret"
+    envsubst < deploy/tls-crt-secret.yaml | kubectl create -f -
+
+    # Use OpenLibertyApplication CR to deploy the sample application
+    envsubst < deploy/ola-k8s-hosted-elasticsearch.yaml | kubectl create -f -
+else
+    # Use deployment/service to deploy the sample application
+    envsubst < deploy/k8s-hosted-elasticsearch.yaml | kubectl create -f -
+fi
 
 echo "The application is succesfully deployed to project ${NAMESPACE}!"
